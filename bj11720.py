2b1b74c97d87d84c448db086d33200f380c246d0
BackMATHedu <152682918+BackMATHedu@users.noreply.github.com>
2023-12-02 17:48:01 +0900

    Update bj11720.py

--- bj11720.py
+++ bj11720.py
@@ -1,11 +1,14 @@
 import sys
 input = sys.stdin.readline
 
+# 입력
 N = int(input())
 array_num = input().strip()
 
+# 실행 코드
 result = 0
 for i in array_num:
     result += int(i)
 
+# 결과물 출력
 print(result)
