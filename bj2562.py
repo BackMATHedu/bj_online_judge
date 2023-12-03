1750840b4d3891e53cb532cb1ae12fd497578f5d
BackMATHedu <152682918+BackMATHedu@users.noreply.github.com>
2023-12-03 10:49:36 +0900

    Update bj2562.py

--- bj2562.py
+++ bj2562.py
@@ -3,13 +3,13 @@ import sys
 input = sys.stdin.readline
 
 # 입력 및 구현
-max = float("-inf") # max 기본값을 -infinite로 세팅
+M = float("-inf") # M 기본값을 -infinite로 세팅
 for i in range(9):
     num_input = int(input())
-    if num_input >= max: # max보다 크거나 같은 경우에 max을 변경하고 index 저장
-        max = num_input
+    if num_input >= M: # M보다 크거나 같은 경우에 M을 변경하고 index 저장
+        M = num_input
         index = i+1
 
 # 결과물 출력
-print(max)
+print(M)
 print(index)
